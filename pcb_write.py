9fa1b5e27174b6a10fc8bd6dab9e97020bd9b601
Glazun PI <blizardboss@gmail.com>
2023-02-24 09:03:40 +0300

    Add the config class for option

--- pcb_write.py
+++ pcb_write.py
@@ -1,20 +1,29 @@
 import svgwrite
 import math
-
 from svgwrite import mm
 
-track_width = 1
-track_to_track_distance = 1
-plate_width = 200
-plate_height = 200
-padding_w = 10
-padding_h = 10
-w = plate_width-padding_w*2
-h = plate_height-padding_h*2
-track_count = int(w/(track_width + track_to_track_distance))
-copper_thiknes_um = 18
-u=220
-temperature=100
+class BoardConfig:
+	def __init__(self,
+				track_width = 1,
+				track_to_track_distance = 1,
+				plate_width = 200,
+				plate_height = 200,
+				padding_w = 10,
+				padding_h = 10,
+				copper_thiknes_um = 18,
+				u=220,
+				temperature=100):
+		self.track_width = track_width
+		self.track_to_track_distance = track_to_track_distance
+		self.plate_width = plate_width
+		self.plate_height = plate_height
+		self.padding_w = padding_w
+		self.padding_h = padding_h
+		self.w = plate_width-padding_w*2
+		self.h = plate_height-padding_h*2
+		self.copper_thiknes_um = copper_thiknes_um
+		self.u = u
+		self.temperature = temperature
 
 def draw_line(dwg, start, end, width):
 	dwg.add(dwg.line(start=(start[0]*mm, start[1]*mm),
@@ -23,7 +32,7 @@ def draw_line(dwg, start, end, width):
 					stroke_width=width*mm,
 					stroke_linecap='round'))
 
-def calc_resistance(lenth, width, thiknes, temp=temperature):
+def calc_resistance(lenth, width, thiknes, temp):
 	#lenth im meters
 	#width im mm
 	#thiknes in um
@@ -38,7 +47,7 @@ def calc_distance(x1, y1, x2, y2):
 		return  math.sqrt(pow(x1-x2, 2)) + math.sqrt(pow(y1-y2, 2))
 
 def calc_track_count(width, track_width, track_to_track_distance):
-	return int(w/(track_width + track_to_track_distance))
+	return int(width/(track_width + track_to_track_distance))
 
 def get_lines_type1(w, h, padding_w, padding_h, track_width, track_to_track_distance):
 	track_count = calc_track_count(w, track_width, track_to_track_distance)
@@ -64,28 +73,34 @@ def get_lines_type1(w, h, padding_w, padding_h, track_width, track_to_track_dist
 			lines.append(line)
 	return lines
 
-def draw_pcb():
+def draw_pcb(config):
 	wire_lenth = 0
-	dwg = svgwrite.Drawing('pcb.svg', profile='tiny', size=(plate_width*mm, plate_height*mm))
-	lines = get_lines_type1(w, h, padding_w, padding_h, track_width, track_to_track_distance)
+	dwg = svgwrite.Drawing('pcb.svg', profile='tiny', size=(config.plate_width*mm, config.plate_height*mm))
+	lines = get_lines_type1(config.w,
+							 config.h,
+							 config.padding_w,
+							 config.padding_h,
+							 config.track_width,
+							 config.track_to_track_distance)
 	wire_lenth = 0
 	for line in lines:
-		draw_line(dwg, line[0], line[1], track_width)
+		draw_line(dwg, line[0], line[1], config.track_width)
 		wire_lenth = wire_lenth + calc_distance(line[0][0], line[0][1], line[1][0], line[1][1])
 	dwg.save()
 	return wire_lenth
 
-def print_info(wire_lenth ):
-	print('Coper thiknes is: ', copper_thiknes_um, 'um')
-	print('Track width is: ', track_width, 'mm')
-	print('Track to track distamce is: ', track_to_track_distance, 'mm')
+def print_info(config, wire_lenth):
+	print('Coper thiknes is: ', config.copper_thiknes_um, 'um')
+	print('Track width is: ', config.track_width, 'mm')
+	print('Track to track distamce is: ', config.track_to_track_distance, 'mm')
 	print('Totol lenth is: ', wire_lenth, 'mm')
-	r = calc_resistance(wire_lenth/1000, track_width, copper_thiknes_um)
-	print('Resistance is: ', r, 'Om at ', temperature, ' degrees of Celsius')
-	print('Current is :', u/r, 'Amps')
-	print('Power is: ', u*u/r, 'Watt')
+	r = calc_resistance(wire_lenth/1000, config.track_width, config.copper_thiknes_um, config.temperature)
+	print('Resistance is: ', r, 'Om at ', config.temperature, ' degrees of Celsius')
+	print('Current is :', config.u/r, 'Amps')
+	print('Power is: ', config.u*config.u/r, 'Watt')
 
 if __name__ == '__main__':
-	wire_lenth = draw_pcb()
-	print_info(wire_lenth )	
+	config = BoardConfig()
+	wire_lenth = draw_pcb(config)
+	print_info(config, wire_lenth)	
 	
